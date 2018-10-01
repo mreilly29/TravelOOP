51b5aabdf034a68a77659c92f01407957ad7b515
mreilly29 <mreilly18@jcu.edu>
2018-10-01 19:55:34 -0400

    first 5 tests passed. Stuck on total number of travel days method

--- TravelOOP.Tests/TravelOOP.Tests/TravelTests.cs
+++ TravelOOP.Tests/TravelOOP.Tests/TravelTests.cs
@@ -10,5 +10,72 @@ namespace TravelOOP.Tests
     [TestFixture]
     public class TravelTests
     {
+        [Test]
+        public void Test_Default_CTR()
+        {
+            //arrange
+            Travel traveller = new Travel("Harry");
+            //action
+            string response = traveller.FName;
+            //assert
+            Assert.That(response, Is.EqualTo("Harry"));
+        }
+
+        [Test]
+        public void Test_LastName_Prop()
+        {
+            //arrange
+            Travel traveller = new Travel("Harry Buffalo");
+            //action
+            string response = traveller.LName;
+            //assert
+            Assert.That(response, Is.EqualTo("Buffalo"));
+        }
+
+        [Test]
+        public void Test_Address_Prop()
+        {
+            //arrange
+            Travel traveller = new Travel("Harry Buffalo");
+            //action
+            traveller.Address=" ";
+            string response = traveller.Address;
+            //assert
+            Assert.That(response, Is.EqualTo(" "));
+        }
+
+        [Test]
+        public void Test_Address_Email()
+        {
+            //arrange
+            Travel traveller = new Travel("Harry Buffalo");
+            //action
+            traveller.Email = " ";
+            string response = traveller.Email;
+            //assert
+            Assert.That(response, Is.EqualTo(" "));
+        }
+
+        [Test]
+        public void Test_Date_Travel_Method()
+        {
+            //arrange
+            Travel traveller = new Travel();
+            //action
+            var response = traveller.GetDate(9, 8, 2018);
+            //assert
+            Assert.That(response, Is.EqualTo("9/8/2018"));
+        }
+
+        [Test]
+        public void Test_Total_Travel_Time_Method()
+        {
+            //arrange
+            Travel traveller = new Travel();
+            //action
+           var response = traveller.TtlTravel(9, 13, 2018);
+            //assert
+            Assert.That(response, Is.EqualTo(5));
+        }
     }
 }
---
+++ TravelOOP.Tests/TravelOOP/Travel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TravelOOP
+{
+    public class Travel
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+        public Travel(string name )
+        {           
+            var words = name.Split(' ');
+            if(words.Length == 1)
+            {
+                FName = words[0];
+            }
+            else
+            {
+                FName = words[0];
+                LName = words[1];
+            }        
+        }
+        public Travel()
+        {
+
+        }
+
+        public string GetDate(int month, int day, int year)
+        {
+            DateTime date = new DateTime(year, month, day);           
+            string value = (month.ToString() + "/" + day.ToString() + "/" + year.ToString());
+            return value;
+        }
+
+        public void TtlTravel(int month, int day, int year)
+        {
+            var startDate = "9/8/2018";
+            var temps = startDate.Split('/');
+            int[] num = new int[3];
+            for (int i = 0; i < temps.Length; i++)
+            {
+                num[i] = int.Parse(temps[i]);
+            }
+
+        }
+    }
+}
